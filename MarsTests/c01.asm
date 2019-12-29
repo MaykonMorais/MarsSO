4e9e11d152229f2c933542fc1e20d5af47da7721
= <maykon.estudos@gmail.com>
2019-12-29 16:20:16 -0300

    implementing labels

--- MarsTests/c01.asm
+++ MarsTests/c01.asm
@@ -2,37 +2,47 @@
 
 # Calcula f = (g+h)-(i+j)
 .data
-      g: .word  5	# valor 5 armazenado em g
-      h: .word  2	# valor 2 armanazenado em h
-      i: .word  1	# valor 1 armanazenado em i
-      j: .word  3	# valor 3 armanazenado em j
-      f: .word  0	# valor 0 armanazenado em f
+      str1: .asciiz "Digite o valor de g: "
+      str2: .asciiz "Digite o valor de h: "
+      str3: .asciiz "Digite o valor de i: "
+      str4: .asciiz "Digite o valor de j: "
+      f: .word 
 
 .text
 .globl main
 main: 
-      # carrengando os valores da memoria
-      la  $t0, g        # carrega o endereco de g em $t0
-      lw  $s1, 0($t0)	# carrega o valor de g em $s1
-      la  $t0, h        # carrega o endereco de h em $t0
-      lw  $s2, 0($t0)	# carrega o valor de h em $s2
-      la  $t0, i        # carrega o endereco de i em $t0
-      lw  $s3, 0($t0)	# carrega o valor de i em $s3
-      la  $t0, j        # carrega o endereco de j em $t0
-      lw  $s4, 0($t0)	# carrega o valor de j em $s4
+      la $a0, str1 # print str1
+      jal print_string
+      jal read_int # read first number
+      move $s1, $v0 # set value of variable g
+      
+      la $a0, str2 # print str2
+      jal print_string
+      jal read_int # read second number
+      move $s2, $v0 # set value of variable h
+      
+      la $a0, str3 # print str3
+      jal print_string
+      jal read_int # read third number
+      move $s3, $v0 # set value of variable i
+            
+      la $a0, str4 # print str4
+      jal print_string
+      jal read_int # read fourth number
+      move $s4, $v0 # set value of variable j
       
       #Realizando o calculo
-      add $t0, $s1, $s2	#soma g e h
-      add $t1, $s3, $s4	#soma i e j
-      sub $s0, $t0, $t1	#(g+h)-(i+j)
+      add $t0, $s1, $s2	#sum of g and h
+      add $t1, $s3, $s4	#sum of i and j
+      sub $s0, $t0, $t1	#f = (gs+h)-(i+j)
       
-      #Armazena o resultado na memoria
+      #store result on memory
       la  $t0, f        # carrega o endereco de f em $t0
       sw $s0, 0($t0)	# armazena o valor
       
       add $a0, $s0, $zero #copiando o valor de $s0 para $a0
-      li $v0, 1		#imprimir inteiro
-      syscall
+      
+      jal print_int
       
       jal done
       
--- MarsTests/macros.asm
+++ MarsTests/macros.asm
@@ -1,18 +1,62 @@
+.data
+	errorString: .asciiz "Erro! numero negativo"
+
+.text
 done: #para finalizar programa
 	li $v0, 10
-	syscall
-	
+	syscall	
 
 print_int: #para imprimir inteiro
-	
+      li $v0, 1		#imprimir inteiro
+      syscall
+      jr $ra	
 
 print_char: #para imprimir caracter
-
+	li $v0, 11
+	syscall
+	jr $ra
+	
 print_string: #para imprimir 
+	li $v0, 4
+	syscall
+	jr $ra
 
 read_int: #para leitura de inteiro
+	addi $sp, $sp, -4
+        sw $ra, 0($sp)
 
+	li $v0, 5
+	syscall
+	jal return
+	
+	lw $ra, 0($sp) # restore $ra register 
+	addi $sp, $sp, 4
+	
+	jr $ra
+	
 read_char: #para leitura de inteiro
+	li $v0, 12
+	syscall
+	jr $ra
+	
+read_string: #para leitura de string
+	li $v0, 8 
+	syscall
+	jr $ra
 
-read_string: #para leitura de inteiro
+return: 
+	blt $v0, 0, exit2 # compare if number is less than 0
+	jr $ra
+	#jal done # return to the end of program? this doesn't make sense 
+	
+exit2: 	
+	la $a0, errorString # load adress message
+	jal print_string # show error message
+	jal done # stop program 
+	
+load_var: # manipulate variables
+
+
+store_var: # manipulate variables
+
 
