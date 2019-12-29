f2d4ffb9dd0f7e0dcb4d1b8c527a4286f0c6de6e
= <maykon.estudos@gmail.com>
2019-12-29 12:06:33 -0300

    adding macros and test done subrotine

---
+++ MarsTests/macros.asm
@@ -0,0 +1,18 @@
+done: #para finalizar programa
+	li $v0, 10
+	syscall
+	
+
+print_int: #para imprimir inteiro
+	
+
+print_char: #para imprimir caracter
+
+print_string: #para imprimir 
+
+read_int: #para leitura de inteiro
+
+read_char: #para leitura de inteiro
+
+read_string: #para leitura de inteiro
+
